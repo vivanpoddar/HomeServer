63b367e7de815cf7b5660299fa2922ca92be68e7
vivanpoddar <vivanneil@outlook.com>
2022-07-06 21:30:01 -0400

    select destination

--- src/Client.java
+++ src/Client.java
@@ -4,19 +4,17 @@ import java.net.*;
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = null;
-        DataInputStream input = new DataInputStream(System.in);
-        System.out.println("Select destination computer");
-        String host = String.valueOf(input.readLine());
-        System.out.println("Select destination port");
-        int port = Integer.parseInt(String.valueOf(input.readLine()));
+        socket = new Socket(String.valueOf(args[0]), Integer.parseInt(args[1]));
 
-        socket = new Socket(host, port);
-        System.out.println("Connected to " + host + ":" + port);
+        System.out.println("Connected to " + args[0] + ":" + args[1]);
         DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-        System.out.println("File pathname");
-        String pathname = String.valueOf(input.readLine());
-        File file = new File(pathname);
+        System.out.println("Choose destination target");
+        DataInputStream input = new DataInputStream(System.in);
+        String line = input.readLine();
+        output.writeUTF(line);
+
+        File file = new File(String.valueOf(args[2]));
         // Get the size of the file
         long length = file.length();
         byte[] bytes = new byte[16 * 1024];
